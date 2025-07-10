e66daa1424c990dc2f20f9ec2bbca1fc65d7d681
MohammadAsif34 <shadowforever34@gmail.com>
2025-07-11 02:17:31 +0530

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const api = axios.create({
   baseURL: "http://localhost:8800/api4/chat/v1/",
+  // baseURL: "https://basic-chat-app-n5s8.onrender.com/api4/chat/v1/",
   withCredentials: true,
 });
 export default api;
